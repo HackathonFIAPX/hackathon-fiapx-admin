0bc07e76e8e8025552328c4aadcbb1a402b5f2ae
GregoriSimei <gregorisimei@gmail.com>
2025-06-28 22:50:59 -0300

    chore: adjust content type

--- src/infra/aws/s3/IS3Handler.ts
+++ src/infra/aws/s3/IS3Handler.ts
@@ -1,19 +1,4 @@
-export type TUploadType = 'video'
-
-export type TAvailableVideoUploadFileType = 'mp4'
-
-export type TGetPresignedUrlParams = {
-    expiresIn?: number
-    uploadType: TUploadType
-    fileName: string
-    fileType: TAvailableVideoUploadFileType
-    contentLength: number
-}
-
-export type TGetPresignedUrlResponse = {
-    url: string
-    key: string
-}
+import { TGetPresignedUrlParams, TGetPresignedUrlResponse } from "./TS3Handler";
 
 export interface IS3Handler {
     getPresignedUrl (input: TGetPresignedUrlParams): Promise<TGetPresignedUrlResponse>
--- src/infra/aws/s3/S3Handler.ts
+++ src/infra/aws/s3/S3Handler.ts
@@ -1,8 +1,9 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import { IS3Handler, TGetPresignedUrlParams, TGetPresignedUrlResponse } from "./IS3Handler";
+import { TVideoUploadFileType, TGetPresignedUrlParams, TGetPresignedUrlResponse } from "./TS3Handler";
 import { envAWS } from "@config/variables/aws";
 import { envS3 } from "@config/variables/s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { IS3Handler } from "./IS3Handler";
 
 const s3Client = new S3Client({
     region: envAWS.region,
@@ -14,15 +15,24 @@ const s3Client = new S3Client({
 });
 
 export class S3Handler implements IS3Handler {
+    private availableContentTypes: { [K in TVideoUploadFileType]: string } = {
+        mp4: 'video/mp4',
+    }
+
     public async getPresignedUrl(input: TGetPresignedUrlParams): Promise<TGetPresignedUrlResponse> {
         const { uploadType, fileName, fileType, contentLength, expiresIn } = input;
 
         const key = `${uploadType}/${fileName}.${fileType}`;
+        const contentType = this.availableContentTypes[fileType];
+
+        if (!contentType) {
+            throw new Error(`Unsupported file type: ${fileType}`);
+        }
         
         const command = new PutObjectCommand({
             Bucket: envS3.bucketName,
             Key: key,
-            ContentType: `video/${fileType}`,
+            ContentType: contentType,
             ContentLength: contentLength,
         })
 
