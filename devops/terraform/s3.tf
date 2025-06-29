9357b98f69b194a2acba658a2be0ac1133c5a195
GregoriSimei <gregorisimei@gmail.com>
2025-06-29 00:56:03 -0300

    fix: video upload

--- devops/terraform/s3.tf
+++ devops/terraform/s3.tf
@@ -8,25 +8,8 @@ resource "aws_s3_bucket" "video_upload_bucket" {
   }
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "zip_expiration_rule" {
-  bucket = aws_s3_bucket.meu_bucket_uploads_curtas.id
-
-  rule {
-    id     = "exclude_zip_images_after_3_days"
-    status = "Enabled"
-
-    filter {
-      prefix = "temp_zips/"
-    }
-
-    expiration {
-      days = 3
-    }
-  }
-}
-
 resource "aws_s3_bucket_lifecycle_configuration" "video_expiration_rule" {
-  bucket = aws_s3_bucket.meu_bucket_uploads_curtas.id
+  bucket = aws_s3_bucket.video_upload_bucket.id
 
   rule {
     id     = "exclude_videos_after_1_days"
