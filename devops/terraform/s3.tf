638bb053fb5c739a086e83c6fa5a2feb4690e930
GregoriSimei <gregorisimei@gmail.com>
2025-07-01 22:15:45 -0300

    ci: create sns and s3 notification event

--- devops/terraform/s3.tf
+++ devops/terraform/s3.tf
@@ -24,3 +24,39 @@ resource "aws_s3_bucket_lifecycle_configuration" "video_expiration_rule" {
     }
   }
 }
+
+resource "aws_s3_bucket_notification" "video_upload_notification" {
+  bucket = aws_s3_bucket.video_upload_bucket.id
+
+  topic {
+    topic_arn     = aws_sns_topic.video_upload_topic.arn
+    events        = ["s3:ObjectCreated:Put"]
+    filter_prefix = "temp_videos/"
+    filter_suffix = ".mp4"
+  }
+
+  depends_on = [aws_sns_topic_policy.allow_s3_publish]
+}
+
+resource "aws_sns_topic_policy" "allow_s3_publish" {
+  arn = aws_sns_topic.video_upload_topic.arn
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "s3.amazonaws.com"
+        },
+        Action = "SNS:Publish",
+        Resource = aws_sns_topic.video_upload_topic.arn,
+        Condition = {
+          ArnLike = {
+            "aws:SourceArn" = aws_s3_bucket.video_upload_bucket.arn
+          }
+        }
+      }
+    ]
+  })
+}
---
+++ devops/terraform/sns.tf
@@ -0,0 +1,3 @@
+resource "aws_sns_topic" "video_upload_topic" {
+  name = "video-upload-topic"
+}
