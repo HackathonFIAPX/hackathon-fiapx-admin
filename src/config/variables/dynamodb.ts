00c160b2f1741c4c7ad4ad04ff275ec5bc9e1bac
GregoriSimei <gregorisimei@gmail.com>
2025-07-09 02:18:06 -0300

    fix: table name var

--- src/config/variables/dynamodb.ts
+++ src/config/variables/dynamodb.ts
@@ -5,5 +5,5 @@ export const envDynamoDB = Object.freeze({
     endpoint: env.get('DYNAMODB_HOST').asString() as string,
     accessKeyId: env.get('DYNAMODB_ACCESS_KEY_ID').asString() as string,
     secretAccessKey: env.get('DYNAMODB_SECRET_ACCESS_KEY').asString() as string,
-    tableName: env.get('DYNAMODB_DB').asString() as string
+    tableName: env.get('DYNAMODB_TABLE').asString() as string
 })
