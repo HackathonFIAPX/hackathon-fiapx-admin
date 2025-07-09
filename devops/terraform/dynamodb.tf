1257450d6594f039ac0e2cec848410a954f132fb
GregoriSimei <gregorisimei@gmail.com>
2025-07-09 01:55:54 -0300

    ci: create GSI to dynamo

--- devops/terraform/dynamodb.tf
+++ devops/terraform/dynamodb.tf
@@ -8,6 +8,17 @@ resource "aws_dynamodb_table" "example" {
     type = "S"
   }
 
+  attribute {
+    name = "clientId"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name               = "clientId-index"
+    hash_key           = "clientId"
+    projection_type    = "ALL"
+  }
+
   tags = {
     Environment = "dev"
     Projeto     = "fiapx-admin"
